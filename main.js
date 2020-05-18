4c090f576a818b8c4a366c8e488b5d93a4847109
Rose-Mead <rmkmead@hotmail.com>
2020-05-19 07:12:28 +1200

    Add function to get the number on click

--- main.js
+++ main.js
@@ -8,3 +8,45 @@
 // if symbol is +-/* do the math on the next number in array
 // if a negative number do math abs to it is handled correctly
 // else push the number on to temp and val
+
+var cells = []
+cells = document.getElementsByTagName('td')
+console.log(cells)
+var array =[1,2,3]
+
+var pressed = 0
+var equation = []
+var symbol = []
+
+
+
+for (var i=1; i<cells.length; i++) {
+    cells[i].onclick =  getNumber(i)
+}
+
+function getNumber(i) {
+    return function() {
+        pressed = cells[i].innerHTML
+        console.log(pressed)
+        addToEquation()
+        displayValue()
+    }
+}
+
+// if number add to array
+// if symbol convert to symbol
+function addToEquation() {
+    if (!isNaN(pressed) || pressed == '=') {
+        equation.push(Number(pressed))
+        console.log(equation)
+    } else if (pressed == '+') {
+        symbol = +
+        console.log(symbol)
+    }
+
+}
+
+function displayValue() {
+    cells[0].innerHTML = pressed
+}
+
