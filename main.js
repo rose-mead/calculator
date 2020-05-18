be99aac86aaed4d28b6bf6ece27ec2b3f37256a6
Rose-Mead <rmkmead@hotmail.com>
2020-05-18 20:30:55 +1200

    Add pseudocode

---
+++ main.js
@@ -0,0 +1,10 @@
+// Reverse pseudocode
+
+// declare variables
+// on button click - store value
+// if value is a number or equal sign - add number to temp variable
+// store as substring in property of answer object 
+// if symbol is clicked - clear variables, convert x to * etc
+// if symbol is +-/* do the math on the next number in array
+// if a negative number do math abs to it is handled correctly
+// else push the number on to temp and val
