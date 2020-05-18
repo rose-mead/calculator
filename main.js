f46a6feb11d233a343aa582e8d4a2e1526345e6b
Rose-Mead <rmkmead@hotmail.com>
2020-05-19 09:13:18 +1200

    Store user input as string

--- main.js
+++ main.js
@@ -1,22 +1,27 @@
 // Reverse pseudocode
 
-// declare variables
-// on button click - store value
-// if value is a number or equal sign - add number to temp variable
+// declare variables - entries array, total num, temp string
+// on button click - store value in temp
+// if value is a number or dot - add number to temp variable
 // store as substring in property of answer object 
-// if symbol is clicked - clear variables, convert x to * etc
+// if symbol is clicked - clear variables, convert x to * etc push onto entries array
 // if symbol is +-/* do the math on the next number in array
 // if a negative number do math abs to it is handled correctly
 // else push the number on to temp and val
 
+//Improvements
+// get pressed key as string
+// use this instead of i with onclick function
+// instead of adding to array += to string
 var cells = []
 cells = document.getElementsByTagName('td')
 console.log(cells)
 var array =[1,2,3]
 
-var pressed = 0
+var pressed = ""
 var equation = []
 var symbol = []
+var array = []
 
 
 
@@ -26,22 +31,21 @@ for (var i=1; i<cells.length; i++) {
 
 function getNumber(i) {
     return function() {
-        pressed = cells[i].innerHTML
+        pressed += cells[i].innerHTML
         console.log(pressed)
-        addToEquation()
-        displayValue()
+        getUserInput()
     }
 }
 
 // if number add to array
 // if symbol convert to symbol
-function addToEquation() {
-    if (!isNaN(pressed) || pressed == '=') {
-        equation.push(Number(pressed))
-        console.log(equation)
-    } else if (pressed == '+') {
-        symbol = +
-        console.log(symbol)
+// pressed = [ [1,2,3], [x], [2]]
+function getUserInput() {
+    if (!isNaN(pressed) || pressed == '.') {
+        displayValue()
+        equation = Number(pressed)
+        console.log(pressed)
+        // displayValue()
     }
 
 }
